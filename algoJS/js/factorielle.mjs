a1eb2f1af862aff4fb81ae9329f643a5774af036
arfpchatelot <42768666+arfpchatelot@users.noreply.github.com>
2026-06-29 11:44:05 +0200

    Ajoute de la fonction recursive factorielle

---
+++ algoJS/js/factorielle.mjs
@@ -0,0 +1,27 @@
+import { createInterface } from "readline/promises";
+import { stdin as input, stdout as output } from "process";
+import strict from "assert/strict";
+async function main() {
+
+    const sc = new createInterface({ input, output });
+
+    let nbTest= parseInt( await sc.question("Veuillez saisir un nombre entier positif <100 !!: "));
+    // application de la fonction factorielle
+    let result= calculFactorielle(nbTest);
+    
+    console.log(` le nombre ${nbTest} passé en argument de la fonction factorielle donnera : ${result} `);
+    
+    sc.close();
+}
+//une fonction récursive est une fonction qui s'appelle elle-même jusqu'à une limite ici nombreEntier>1.  
+function calculFactorielle( nombreEntier)
+{
+if (nombreEntier>1) {
+     return nombreEntier * calculFactorielle(nombreEntier - 1);
+} else {
+    return 1
+}
+
+}
+
+await main();
