81a165b1b0415ae495c8b47b09f7b3287348a434
arfpchatelot <42768666+arfpchatelot@users.noreply.github.com>
2026-07-01 10:32:25 +0200

    Ajout exercice jeu du pendu avec utilisation de fonctions

--- algoJS/js/jeupendu.mjs
+++ algoJS/js/jeupendu.mjs
@@ -1,20 +1,84 @@
 import { createInterface } from "readline/promises";
 import { stdin as input, stdout as output } from "process";
+import { table } from "console";
 
 async function main() {
-let tabmotjoueur=[];
   const sc = new createInterface({ input, output });
-let motjoueur1= await sc.question("Veuillez entrer un nom commun à rechercher !!");
- tabmotjoueur= [...motjoueur1];
-for (let i = 1; i < tabmotjoueur.length-1; i++) {
-   
-   tabmotjoueur[i]='-'; 
-}
-console.clear();
+  let motjoueur1 = await sc.question(
+    "Veuillez entrer un nom commun à rechercher !!",
+  );
 
-console.log(" le mot à deviner est le suivant \n ");
-console.log(tabmotjoueur);
-sc.close();
+  console.clear();
+  motjoueur1 = motjoueur1.toLowerCase();
+  let textepropre = motjoueur1.replaceAll(/[éèêë]/g, "e");
+
+  let tabmotjoueur = [...textepropre];
+
+  for (let i = 1; i < tabmotjoueur.length - 1; i++) {
+    tabmotjoueur[i] = "-";
+  }
+  console.log(" le mot à deviner est le suivant \n ");
+  console.log(afficherTableau(tabmotjoueur));
+  //console.table(tabmotjoueur);
+  //console.clear();
+
+  let trouve = false;
+  let essai = 0;
+  let lettre = "";
+  do {
+    lettre = await sc.question(
+      "Veuillez choisir une lettre qui compose le mot ou pas !!",
+    );
+    if (trouveLettre(tabmotjoueur, lettre, textepropre) == false) {
+      essai++;
+      if (6 - essai > 0) {
+        console.log(
+          "Il ne vous reste plus que : " + (6 - essai) + " coup(s) à jouer !!",
+        );
+      }
+    } else {
+      // console.log(tabmotjoueur);
+      trouve = true;
+      for (let i = 1; i < tabmotjoueur.length - 1; i++) {
+        if (tabmotjoueur[i] == "-") {
+          trouve = false;
+        }
+      }
+    }
+  } while (essai < 6 && trouve == false);
+  if (trouve == true) {
+    let chainemot = tabmotjoueur.join("");
+    console.log(
+      `Bravo !! Vous avez gagné et trouvé le mot qui était : ${chainemot} en ${essai} erreur(s)  !!`,
+    );
+  } else {
+    console.log(
+      "Vous avez dépassé le nombre d'essais requis pour deviner le mot (6 erreurs maxi !) vous êtes pendu !!",
+    );
+    console.log("vous pouvez rejouer une partie avec un autre mot deviner !");
+  }
+
+  sc.close();
 }
 
+function trouveLettre(_tabmotjoueur, lettre, mot) {
+  let win = false;
+  for (let i = 1; i < mot.length - 1; i++) {
+    if (lettre == mot[i]) {
+      _tabmotjoueur[i] = lettre;
+      win = true;
+    }
+  }
+  console.log(afficherTableau(_tabmotjoueur));
+  return win;
+}
+function afficherTableau(_montab) {
+  let tabaffichage = "[";
+  for (const el of _montab) {
+    tabaffichage += el + " ,";
+  }
+  tabaffichage = tabaffichage.substring(0, tabaffichage.length - 1);
+  tabaffichage += "]";
+  return tabaffichage;
+}
 await main();
